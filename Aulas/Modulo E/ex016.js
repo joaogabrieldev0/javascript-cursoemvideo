95a673885b82d90f4293b53b02ef55ed3c78366b
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2025-05-21 15:57:34 -0300

    Aprendizado em JavaScript - 15:57 - 21/05/2025

---
+++ Aulas/Modulo E/ex016.js
@@ -0,0 +1,25 @@
+var c = 1
+
+// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INÍCIO
+
+while (c <= 15){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL
+
+do{
+
+console.log(`Passo ${c}`)
+c++
+
+} while (c <= 16)
+
+
+
+
+
+
+
